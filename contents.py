5d39c80b910b9ba76bd01566ca0b436d062fc44e
billybob <p6kmym8fyw@privaterelay.appleid.com>
2026-09-16 12:28:01 -0700

    Update input method for list elements
    Reconfigured logic to allow users to input all values concurrently instead of individually.

---
+++ contents.py
@@ -0,0 +1,9 @@
+'''
+n = int(input("Enter the number of elements: "))
+a = [input(f"Enter element {i+1}: ") for i in range(n)]
+print("List:", a)
+
+program returns the average of a individually inputted series of values.
+reconfigure logic to allow users to input all values concurrently.
+
+09/16
